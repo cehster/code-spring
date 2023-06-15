4d04a519c54b52874dc47d894a6e6835d9a3d52e
Chris Ehster <cehster@pm.me>
2023-06-14 22:53:43 -0400

    work with state and hooks

--- code-spring/src/Home.js
+++ code-spring/src/Home.js
@@ -1,18 +1,23 @@
+import { useState } from 'react';
+
 const Home = () => {
 
-    const handleClick = () => {
-        console.log('hello!');
-    }
+    const [name, setName] = useState("Mario");
+    const [age, setAge] = useState(25);
 
-    const handleClickAgain = (name, e) => {
-        console.log('hello ' + name, e.target);
+    const handleClick = () => {
+        setName("Luigi");
+        setAge(30);
+        console.log('hello!');
     }
 
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <p>Name: { name } </p>
+            <p>Age: { age } years old.</p>
+            <br/>
             <button onClick={handleClick}>Click me!</button>
-            <button onClick={(e) => handleClickAgain("Mario", e)}>Click again!</button>
         </div>
      );
 }
