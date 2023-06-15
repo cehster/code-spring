b8c79f4882ae6c91fdb9d64bfdd531c1d2107412
Chris Ehster <cehster@pm.me>
2023-06-14 22:13:50 -0400

    working with dynamic data in a component

--- code-spring/src/App.js
+++ code-spring/src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "Welcome to My Blog";
+  const likes = 50;
+  const link = "https://www.google.com";
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times.</p>
+
+        {/* testing different js output */}
+        <p>{ 10 }</p>
+        <p>{"hello!"}</p>
+        {/* flattens array  */}
+        <p>{[1, 2, 3]}</p>
+        <p>{Math.random() * 100}</p>
+
+        {/* links and attributes */}
+        <a href={link}>Google</a>
       </div>
     </div>
   );
